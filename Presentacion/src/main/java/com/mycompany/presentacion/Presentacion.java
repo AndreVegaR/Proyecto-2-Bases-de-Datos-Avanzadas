7ad7374f55c843052cfa681f93b3a56f633c60cc
AndreVegaR <abelardo.vega262772@potros.itson.edu.mx>
2026-04-07 15:53:02 -0700

    Ahora se registran mesas

--- Presentacion/src/main/java/com/mycompany/presentacion/Presentacion.java
+++ Presentacion/src/main/java/com/mycompany/presentacion/Presentacion.java
@@ -1,5 +1,9 @@
 package com.mycompany.presentacion;
+import Coordinadores.CoordinadorNegocio;
+import DTOs.MesaDTO;
 import Principal.MenuEmpleados;
+import Utilerias.Constantes;
+import java.util.List;
 import javax.swing.UIManager;
 
 /**
@@ -9,6 +13,22 @@ import javax.swing.UIManager;
 public class Presentacion {
     public static void main(String[] args) {       
         
+        
+        /**
+         * Inserta mesas en el sistema
+         * Si no hay ninguna registrada previamnete, se está creando la BD
+         * Si ya hay por lo menos una mesa, ya se asume que se registró y se ignora
+         */
+        List<MesaDTO> mesas = CoordinadorNegocio.getInstance().consultarMesas();
+        if (mesas.isEmpty()) {
+            for (int i = 1; i <= Constantes.NUMERO_MESAS; i++) {
+                MesaDTO mesa = new MesaDTO();
+                mesa.setNumero(i);
+                CoordinadorNegocio.getInstance().agregarMesa(mesa);
+            }
+        }
+        
+
         //Usa los gráficos nativos del sistema
         //Revisar problemas de color en tabla despues
         /**
